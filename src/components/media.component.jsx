c6fc9957f413ee6340c7ed96305430a802af6077
Md. Abdullahil Kafi <ahkafi2003@gmail.com>
2025-07-30 23:58:56 +0600

    media component responsive

--- src/components/media.component.jsx
+++ src/components/media.component.jsx
@@ -5,12 +5,12 @@ import videoData from '../store/video-data.json';
 
 function Media() {
   return (
-    <div className='flex gap-5 mt-3'>
+    <div className='lg:flex gap-5 mt-3'>
       <MediaPlayer />
 
-      <div className="side-content flex-2">
-        <h1 className=''>Mix-Playlist with the song of favourite</h1>
-        <div className="list_items px-1">
+      <div className="side-content lg:flex-2 lg:mt-0 mt-3">
+        <h1 className='font-medium py-4 bg-zinc-700 px-2 rounded-t-md'>Mix-Playlist with the song of favourite</h1>
+        <div className="list_items p-1 lg:p-2 bg-black h-100 overflow-y-scroll rounded-b-md ">
           {
             videoData.map((video, index) => (
               <Video video={video} key={index} index={index} />
